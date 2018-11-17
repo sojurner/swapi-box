509dbc63f0401443a65e39545ac78724ef3eaf0a
sojurner <pykim0591@gmail.com>
2018-11-16 19:43:43 -0700

    Add provider and store

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import './index.css';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+import rootReducer from './reducers';
 
-import "./index.css";
-import App from "./components/App";
-import registerServiceWorker from "./registerServiceWorker";
+const devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(rootReducer, devTools);
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
